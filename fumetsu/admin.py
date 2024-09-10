44d3cebeb488f3c9803cdc9625e93b5ed5efc823
Piotr <103199955+ccandour@users.noreply.github.com>
2024-09-10 23:26:50 +0200

    feat: added all models to admin site

--- fumetsu/admin.py
+++ fumetsu/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from .models import *
 
 
+admin.site.register(AnimeEpisode)
+admin.site.register(AnimePost)
 admin.site.register(AnimeSeries)
+admin.site.register(Announcement)
+admin.site.register(EpisodeComment)
+admin.site.register(Player)
 admin.site.register(Relation)
 admin.site.register(SeriesComment)
-admin.site.register(Announcement)
-admin.site.register(AnimePost)
-admin.site.register(AnimeEpisode)
-admin.site.register(Player)
+admin.site.register(StaffCredit)
 admin.site.register(Tag)
-admin.site.register(EpisodeComment)
+admin.site.register(UrlRedirect)
 
